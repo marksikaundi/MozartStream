acea2e737b6f597cd52203a80169461e46869954
marksikaundi <emmanuelmark117@gmail.com>
2023-05-06 23:26:21 +0200

    styling

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function MenuItem({title, address, Icon}) {
   return (
     <div>
-      
+      <Link href={address} className='mx-4 lg:mx-6 hover:text-amber-600'>
+        <Icon className="text-2xl" />
+      <p>{title}</p>
+      </Link>
     </div>
   )
 }
