04865efec9de963909495ef6522b8eefcc6a35c9
marksikaundi <emmanuelmark117@gmail.com>
2023-05-06 17:53:45 +0200

    tailwind css testing

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <h1>Home</h1>
-  )
+  return <h1 className="font-bold text-center text-lg">Home</h1>;
 }
