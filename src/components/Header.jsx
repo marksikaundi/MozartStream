010a042bd46ba5ab0752cac4b3bcee99c508b138
marksikaundi <emmanuelmark117@gmail.com>
2023-05-06 18:55:48 +0200

    installed some packages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import MenuItem from './MenuItem'
+import {AiFillHome} from "react-icons/ai"
 
 export default function Header() {
   return (
     <div>
       <div className=''>
-
+        <MenuItem title="Home" address="/" Icon={AiFillHome} />
       </div>
 
       <div className=''>
-        
+
       </div>
     </div>
   )
