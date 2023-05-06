33fadc2baf6248b9ffe0f6e0be347dcf3c2408af
marksikaundi <emmanuelmark117@gmail.com>
2023-05-07 00:52:27 +0200

    dark theme developed

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -1,11 +1,21 @@
+"use client";
+
 import React from "react";
 import { MdLightMode } from "react-icons/md";
 import { BsFillMoonFill } from "react-icons/bs";
+import { useTheme } from "next-themes";
+
 export default function DarkModeSwitch() {
+  const { systemTheme, theme, setTheme } = useTheme();
+
+  const currentTheme = theme === "system" ? systemTheme : theme;
   return (
     <>
-      <MdLightMode />
-      <BsFillMoonFill />
+      {currentTheme === "dark" ? (
+        <MdLightMode onClick={() => setTheme("light")} />
+      ) : (
+        <BsFillMoonFill onClick={() => setTheme("dark")}/>
+      )}
     </>
   );
 }
