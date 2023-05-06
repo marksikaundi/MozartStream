eb93456961a7f720747d3c04131f9fefa384a7e3
marksikaundi <emmanuelmark117@gmail.com>
2023-05-06 23:44:53 +0200

    leftside of header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div>
-      <div className="">
-        <MenuItem title="Home" address="/" Icon={AiFillHome} />
+      <div className="flex">
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
 
-        <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
 
-      <div className=""></div>
+      <div className="">
+        <Link href="/">
+          <h2 className="text-2xl">
+            <span className="font-bold bg-amber-500">IMDB</span>
+            <span>Stream</span>
+          </h2>
+        </Link>
+      </div>
     </div>
   );
 }
