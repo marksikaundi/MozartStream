d6123ad330aed25c150e92a7150fc942e5730d30
marksikaundi <emmanuelmark117@gmail.com>
2023-05-06 18:42:43 +0200

    added menu items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 export default function Header() {
   return (
     <div>
-      Header
+      <div className=''>
+
+      </div>
+
+      <div className=''>
+        
+      </div>
     </div>
   )
 }
