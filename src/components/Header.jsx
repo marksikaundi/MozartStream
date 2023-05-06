fedd3fb17584f21db5771e8422c365ab287555f8
marksikaundi <emmanuelmark117@gmail.com>
2023-05-06 23:34:14 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import MenuItem from './MenuItem'
-import {AiFillHome} from "react-icons/ai"
+import React from "react";
+import MenuItem from "./MenuItem";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
 
 export default function Header() {
   return (
     <div>
-      <div className=''>
+      <div className="">
         <MenuItem title="Home" address="/" Icon={AiFillHome} />
+
+        <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
 
-      <div className=''>
-
-      </div>
+      <div className=""></div>
     </div>
-  )
+  );
 }
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -5,8 +5,8 @@ export default function MenuItem({title, address, Icon}) {
   return (
     <div>
       <Link href={address} className='mx-4 lg:mx-6 hover:text-amber-600'>
-        <Icon className="text-2xl" />
-      <p>{title}</p>
+        <Icon className="text-2xl sm:hidden" />
+      <p className='hidden sm:inline my-2 text-sm'>{title}</p>
       </Link>
     </div>
   )
