a0c5ffa0677135c2e7eb4fa8cb79a510bc07ff5c
marksikaundi <emmanuelmark117@gmail.com>
2023-05-06 18:32:04 +0200

    creating header

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* Header */}
+
+        {/* NavBar */}
+
+        {/* Search */}
+        {children}
+        </body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="font-bold text-center text-lg">Home</h1>;
+  return <h1 className="font-bold text-center text-red-600">Home</h1>;
 }
