57ae81373e5bc49082e89c7aa175fad385e2da72
marksikaundi <emmanuelmark117@gmail.com>
2023-05-06 23:55:11 +0200

    menu done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex justify-between mx-2 max-w-6xl">
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
       <div className="flex">
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
 
