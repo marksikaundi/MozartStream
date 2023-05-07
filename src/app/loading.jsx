f0cad8e8033dc4fc533cad5c3264557d5aa8d782
marksikaundi <emmanuelmark117@gmail.com>
2023-05-07 13:35:15 +0200

    loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="flex justify-center">
+      <imge className="h-96" src="Spin.svg" alt="loading..." />
+    </div>
+  );
+}
