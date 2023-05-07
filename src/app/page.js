49a4d3e4daaead70c0a7c10ec4fc3aa026397666
marksikaundi <emmanuelmark117@gmail.com>
2023-05-07 11:59:20 +0200

    results successsfully fetched data from api

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,27 @@
+import Results from "@/components/Results";
 
 const API_KEY = process.env.API_KEY; //"https://api.themoviedb.org/3/";
 
-export default function Home() {
-  return <h1 className="">Home</h1>;
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if(!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+  const results = data.results;
+  
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  )
 }
