f6d4ba1abf7f2578825e377f884f1b34ac29f688
marksikaundi <emmanuelmark117@gmail.com>
2023-05-07 09:38:09 +0200

    done with navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import NavbarItem from './NavbarItem'
 
 export default function Navbar() {
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center dark:bg-gray-600 bg-amber-100 lg:text-lg p-4'>
       <NavbarItem title="Trending" param="fetchTrending" />
       <NavbarItem title="Top Rated" param="fetchTopRated" />
     </div>
--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,12 +1,25 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React from "react";
 
-export default function NavbarItem({title, param}) {
+import {useSearchParams} from "next/navigation";
+
+export default function NavbarItem({ title, param }) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+
   return (
     <div>
-      <Link href={`/?genre=${param}`}>
+      <Link
+        className={`m-4 hover:text-amber-600 font-semibold p-2 ${
+          genre &&
+          genre === param &&
+          "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+        }`}
+        href={`/?genre=${param}`}
+      >
         {title}
       </Link>
     </div>
-  )
+  );
 }
